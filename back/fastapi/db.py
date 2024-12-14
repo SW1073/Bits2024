1dbb51f39fcea64c13bcfcc34df6106b407b036f
SW1073 <eloiimerino@gmail.com>
2024-12-14 22:43:34 +0100

    Checkpoint

--- back/fastapi/db.py
+++ back/fastapi/db.py
@@ -7,9 +7,10 @@ from form_model import FormModel
 
 
 class DBManager:
-    def __init__(self, path_db, path_schema):
+    def __init__(self, path_db, path_schema, path_src):
         self.path = path_db
         self.schema = path_schema
+        self.csv_src = path_src
         self._connect()
         # self._execute_query("INSERT INTO municipality(id) VALUES (?)", ('Municipi de la fib',));
 
@@ -23,6 +24,7 @@ class DBManager:
 
         if create_tables:
             self._create_tables()
+            self._populate_db_with_csv(self.csv_src)
 
     def _execute_query(self, query, params=None):
         cur = self.con.cursor()
@@ -48,7 +50,7 @@ class DBManager:
                 print(q)
                 self._execute_query(q)
 
-    def populate_db_with_csv(self, csv_path):
+    def _populate_db_with_csv(self, csv_path):
         try:
             df = pd.read_csv(csv_path, encoding = "utf-8")
         except UnicodeDecodeError:
@@ -64,16 +66,4 @@ class DBManager:
             self._execute_query("""
 INSERT INTO daily_log(event_date, fk_id_class, num_alumnes, mal_de_panxa, calfreds, mal_de_cap, mal_de_coll, mocs, nas_tapat, esternut, vomits, altres, be, regular, malament, tos) VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)
 """, (row.timestamp, class_id, row.num_alumnes, row.mal_de_panxa, row.calfreds, row.mal_de_cap, row.mal_de_coll, row.mocs, row.nas_tapat, row.esternut, row.vomits, row.altres, row.be, row.regular, row.malament, row.tos))
-
-
-    def get_alumnes(self, uf_id):
-        # return self._execute_query(f"SELECT * FROM alumne a WHERE a.uf_id == {uf_id}")
-        return self._execute_query(f"SELECT * FROM daily_log")
-
-    def insert_daily_log(self, alumni_id, log: FormModel):
-        class_id = self._execute_query("SELECT fk_id_class FROM alumni a WHERE a.id == ?", (alumni_id,))
-        instance = self._execute_query("SELECT * FROM daily_log d WHERE d.fk_id_class == ? && d.date == ?", (class_id, datetime.now().date()))
-        return instance
-
-        # self._execute_query("INSERT INTO daily_log(date, fk_id_class, num_alumnes, mal_de_panxa, calfreds, mal_de_cap, mal_de_coll, mocs, nas_tapat, esternut, vomits, altres, be, regular, malament, tos) VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)", (datetime.now().date(), class_id, form,))
 
