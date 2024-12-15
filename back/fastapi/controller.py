a249456bb9aae9a21b174b9803611ec4c4858394
SW1073 <eloiimerino@gmail.com>
2024-12-15 09:52:52 +0100

    spelling

--- back/fastapi/controller.py
+++ back/fastapi/controller.py
@@ -8,7 +8,8 @@ class Controller:
     def get_alumnes(self, uf_id):
         return self.db._execute_query(f"SELECT COUNT(*) FROM daily_log")
 
-    def post_daily_log(self, institut, cusr, log: FormModel):
+    def post_daily_log(self, institut, curs, log: FormModel):
         class_id = self.db._execute_query("SELECT id FROM class c where c.centre == ? AND c.curs == ?", (institut, curs))[0][0];
         instance = self.db._execute_query("SELECT * FROM daily_log d WHERE d.fk_id_class == ? && d.date == ?", (class_id, datetime.now().date()))
         print(instance)
+        # instance = self.db._execute_query("", (,))
